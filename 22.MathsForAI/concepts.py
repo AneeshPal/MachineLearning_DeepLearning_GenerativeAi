41e866feea475ba7fb0f27a93446e13fdd2e600e
AneeshPal <2k23.cs2313398@gmail.com>
2026-04-13 19:25:22 +0530

    Maths For AI Concepts

---
+++ 22.MathsForAI/concepts.py
@@ -0,0 +1,8 @@
+# concepts
+# 1.Probability(fundamentals,Bayes,Conditional,Distribution)
+#2.Linear Algebra(vectors,matrices)
+#3.Calculus(differential)
+
+#Probability
+#1.Theoretical Probability
+#2.Empirical(Experimental)Probability)
